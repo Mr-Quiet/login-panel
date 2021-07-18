8b85852d0a155fcbfde1c206dcf46417b5f88726
Vadim-Kliui <kliui.v.s@gmail.com>
2021-07-18 15:04:39 +0300

    Мобильная адаптация

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ table_filter_init();
 
 let select = document.querySelector("select[name='Science degree']");
 select.addEventListener('change', () => {});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+	let burger = document.querySelector('.header__burger');
+	let header_menu = document.querySelector('.header__menu');
+
+	burger.addEventListener('click', function () {
+		this.classList.toggle("active");
+		header_menu.classList.toggle("active");
+	});
+});
